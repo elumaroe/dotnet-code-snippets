8a18ed2cf8077425d89cce2c4faa0d6af48025b1
elumaroe <59798184+elumaroe@users.noreply.github.com>
2020-01-12 07:00:05 -0800

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,34 @@
+﻿ using System;
+
+namespace esther
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int amt = 1000;
+            int current;
+            int pin = 5050;
+            int pin1;
+            int pin2;
+            int f;
+            Console.WriteLine("please insert your ccard");
+             Console.WriteLine("your pin is 5050");
+            Console.WriteLine("Enter your pin");
+            pin2 = int.Parse(Console.ReadLine());
+            if (pin2==pin)
+            {
+                Console.WriteLine("1. To check your balance");
+                Console.WriteLine("2. To withdrawl");
+                Console.WriteLine("3.To change your pin");
+                
+                
+
+
+
+
+            }
+        }
+    }
+}
